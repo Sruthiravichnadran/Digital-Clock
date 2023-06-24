916cb3daeb5aa69017527bf9007003fc7bffcb3b
Sruthiravichnadran <126389806+Sruthiravichnadran@users.noreply.github.com>
2023-06-24 16:17:49 -0400

    Work done

---
+++ source.js
@@ -0,0 +1,27 @@
+
+
+
+let am =  document.getElementById('am')
+
+function displayTime(){
+    let dateTime = new Date();
+    let hr = dateTime.getHours();
+    let min = dateTime.getMinutes();
+    let sec = dateTime.getSeconds();
+
+    if(hr>12){
+        hr = hr - 12
+        am.innerHTML ='PM'
+    }
+
+    document.getElementById('hours') .innerHTML = padZero (hr)
+    document.getElementById('mins') .innerHTML = padZero (min)
+    document.getElementById('sec') .innerHTML = padZero (sec)
+
+}
+
+function padZero(num){
+    return num<10?"0"+num:num
+}
+
+setInterval(displayTime,500)
